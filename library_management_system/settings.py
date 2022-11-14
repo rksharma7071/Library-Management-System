7e3d26005eb00ee5c17dcc8c2343f60c4b3d33f9
rksharma7071 <rksharma7071@gmail.com>
2022-11-14 19:48:36 +0530

    new

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -39,10 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'uni_form'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
